d1e2a6e588f2a7cfcf21ca3ff2ec515f60a79926
zlnnnnn <55903026+zlnnnnn@users.noreply.github.com>
2021-08-09 11:14:14 -0600

    Add files via upload

---
+++ lessonContracts/array.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.6.12;
+
+contract Arr {
+    
+    // fixed array.
+    string[5] age = ["a","b","c","d","e"];
+    
+    // dynamic arrays
+    string[] name;
+    
+    name.push("cat");
+}
