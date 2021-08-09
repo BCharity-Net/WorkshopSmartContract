e9280b90ef4021fae5ba9e6665d83bf51fd21ce4
zlnnnnn <55903026+zlnnnnn@users.noreply.github.com>
2021-08-09 13:25:33 -0600

    Update abstractClass.sol

--- lessonContracts/abstractClass.sol
+++ lessonContracts/abstractClass.sol
@@ -1,32 +1,27 @@
+abstract class
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity 0.6.12;
 
-contract abstractHelloWorld {
-    
-    function GetValue() public view returns (uint);
-    function Setvalue(uint _vaule) public;
-    
-    function addaNumber(uint _vaule) public returns (uint){
-        return 10;
+abstract contract abstractHelloWorld {
+
+    function Getvalue() virtual public view returns (uint);
+    function Setvalue(uint _vaule) virtual public;
+
+    function addaNumber(uint _vaule) virtual public pure returns (uint){
+        return _vaule;
     }
 }
-   
-contract HelloWorld is abstractHelloWorld {
-    uint private simpleInteger;
-    
-    function GetValue() public view returns (uint) {
+
+abstract contract HelloWorld is abstractHelloWorld {
+    uint public simpleInteger;
+
+    function Getvalue() public override view returns (uint) {
         return simpleInteger;
     }
-    
-    function SetValue(uint _value) public {
+
+    function Setvalue(uint _value) public override{
         simpleInteger = _value;
     }
-    
-    function addNumber(uint _value) public returns(uint) {
-        return simpleInteger + _value;
-    }
-} 
- 
 
-
+}
