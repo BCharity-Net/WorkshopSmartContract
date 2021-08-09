1e1bc8a873a770c7fcabd56e59bc31c497939f81
zlnnnnn <55903026+zlnnnnn@users.noreply.github.com>
2021-08-09 11:18:50 -0600

    Create abstractClass.sol

---
+++ lessonContracts/abstractClass.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.6.12;
+
+contract abstractHelloWorld {
+    
+    function GetValue() public view returns (uint);
+    function Setvalue(uint _vaule) public;
+    
+    function addaNumber(uint _vaule) public returns (uint){
+        return 10;
+    }
+}
+   
+contract HelloWorld is abstractHelloWorld {
+    uint private simpleInteger;
+    
+    function GetValue() public view returns (uint) {
+        return simpleInteger;
+    }
+    
+    function SetValue(uint _value) public {
+        simpleInteger = _value;
+    }
+    
+    function AddaNumber(uint _value) public returns(uint) {
+        return simpleInteger + _value;
+    }
+} 
+ 
+
+
